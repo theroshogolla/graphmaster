0fae38a911d7dbf2bbb33f6c391b49e1894d276f
Andrew Yu <asy51@case.edu>
2023-04-18 14:06:33 -0400

    First GCN training pipeline

--- data.py
+++ data.py
@@ -1,25 +1,54 @@
-"""data preproc GNNs"""
-
+"""
+Read ChessDB dataset from
+https://www.kaggle.com/datasets/milesh1/35-million-chess-games
+https://chess-research-project.readthedocs.io/
+"""
+import pandas as pd
 import chess
 import chess.pgn
-from graph import mobility
+import io
 
-def get_graphs(g: chess.pgn.Game, graph_fn=mobility, skip_first_n=10, skip_last_n=10):
+FPATH = '/home/asy51/repos/graphmaster/dataset/all_with_filtered_anotations_since1998.txt'
+
+def parse(fpath=FPATH, nrows=1000) -> pd.DataFrame:
+    df = pd.read_csv(fpath, engine='python', skiprows=4, sep='###', nrows=nrows)
+    df = df.reset_index()
+    newcols = ['id', 'Date', 'Result', 'welo' ,'belo', 'len', 'date_c', 'resu_c',
+               'welo_c', 'belo_c', 'edate_c', 'setup', 'fen', 'resu2_c', 'oyrange', 'bad_len']
+    df[newcols] = df['index'].str.split(' ', expand=True).iloc[:,:-1]
+    df['moves'] = df.iloc[:,1]
+    df['moves'] = df['moves'].str.replace('[WB]\d+?\.', '', regex=True)
+    df = df.iloc[:,2:].set_index('id')
+    
+    df = df[(df['setup'] == 'setup_false') & (df['bad_len'] == 'blen_false') & (df['moves'].notna())]
+    return df
+
+def get_game(row: pd.Series):
+    moves = io.StringIO(row['moves'])
+    game = chess.pgn.read_game(moves)
+    game.headers.update(row.drop('moves'))
+    return game
+
+def get_boards(g: chess.pgn.Game, skip_first_n=10, skip_last_n=10, meta=['win']):
     """
-    chess.pgn.Game -> list of (x=graph_fn(board), y=win_or_loss) for the last_n boards of a game
+    chess.pgn.Game -> list of (dict['board' + meta]) for each move in game
     skips draws
     TODO: use efficiently updating graph generation instead of from scratch each time
     """
-    outcome = g.game().headers['Result']
-    if outcome == '1-0': outcome = True
-    elif outcome == '0-1': outcome = False
-    else: return [] # '1/2-1/2' and '*' (unknown)
+    
+    if isinstance(meta, str): meta = [meta]
+    if 'win' in meta:
+        outcome = g.game().headers['Result']
+        if outcome == '1-0': outcome = True
+        elif outcome == '0-1': outcome = False
+        else: return [] # '1/2-1/2' and '*' (unknown)
     ret = []
     cur = g
     while cur.next() is not None:
-        player = cur.board().turn
-        win = player == outcome
-        graph = graph_fn(cur.board())
-        ret.append((graph, win))
+        item_dict = dict()
+        item_dict['board'] = cur.board()
+        if 'win' in meta:
+            item_dict['win'] = cur.board().turn == outcome
+        ret.append(item_dict)
         cur = cur.next()
     return ret[skip_first_n:-skip_last_n]
--- graph.py
+++ graph.py
@@ -5,25 +5,73 @@ From ChessY https://github.com/mrudolphlilith/ChessY
 """
 
 import chess
+import chess.pgn
 import networkx as nx
 
-def mobility(b: chess.Board, both=False) -> nx.DiGraph:
+"""from AlphaZero https://arxiv.org/abs/2009.04374"""
+PIECE_VALUE = {
+    '-': 0.00, # empty square
+    'P': 1.00, 'p': -1.00,
+    'N': 3.05, 'n': -3.05,
+    'B': 3.33, 'b': -3.33,
+    'R': 5.63, 'r': -5.63,
+    'Q': 9.50, 'q': -9.50,
+    'K': 200.00, 'k': -200.00,
+}
+
+### node attr functions
+def piece_value(b: chess.Board) -> dict:
+    pieces = {i: 0.00 for i in range(64)}
+    # pieces = OrderedDict((i,{'piece': None}) for i in range(64))
+    turn_sign = 1 if b.turn else -1
+    pieces.update({loc: turn_sign * PIECE_VALUE[piece.symbol()] for loc, piece in b.piece_map().items()})
+    return pieces
+
+### edge functions
+def mobility(b: chess.Board, both=False) -> dict:
     """
-    `both`: get graph for both players (urrent turn AND next turn)
+    `both`: get graph for both players (current turn AND next turn)
     node are int locations with 'piece' attribute e.g. `4: {'piece': K}`
+    TODO: separate graph (mobility) & attr (piece) collection
     """
-    moves = [(m.from_square, m.to_square, b.piece_at(m.from_square)) for m in list(b.legal_moves)]
+    moves = [(m.from_square, m.to_square) for m in list(b.legal_moves)]
     if both:
         b.push(chess.Move.null()) # pass turn
-        moves += [(m.from_square, m.to_square, b.piece_at(m.from_square)) for m in list(b.legal_moves)]
+        moves += [(m.from_square, m.to_square) for m in list(b.legal_moves)]
         b.pop() # undo pass turn
-    pieces = {i:{'piece': ' '} for i in range(64)}
-    # pieces = OrderedDict((i,{'piece': None}) for i in range(64))
-    pieces.update({loc:{'piece':piece.symbol()} for loc, piece in b.piece_map().items()})
-    g = nx.DiGraph()
-    g.add_nodes_from(tuple(pieces.items()))
-    g.add_edges_from([e[:2] for e in moves])
-    return g
+    return moves
 
-def support(b: chess.Board, both=False) -> nx.DiGraph:
-    pass
+# def support(b: chess.Board, both=False) -> nx.DiGraph:
+#     pass
+
+# def support(b: chess.Board):
+#     """dict of {from-piece}"""
+#     piece_map = b.piece_map()
+#     return {piece: [(target, piece_map[piece].color == piece_map[target].color)
+#                 for target in b.attacks(piece).intersection(piece_map.keys())]
+#                     for piece in piece_map}
+
+# x = mobility(b)
+# y = piece_value(b)
+# y = {loc:{'val': val} for loc, val in y.items()}
+# g = nx.DiGraph()
+# g.add_nodes_from(range(64))
+# g.add_edges_from(x)
+# nx.set_node_attributes(g, y)
+# nx.draw(g, {i: divmod(i, 8)[::-1] for i in range(64)}, labels=nx.get_node_attributes(g, 'val'))
+
+def chess_nx(b: chess.Board, edge_fn=mobility, node_fns=piece_value):
+    g = nx.DiGraph()
+    g.add_nodes_from(range(64))
+    g.add_edges_from(edge_fn(b))
+
+    if not isinstance(node_fns, (list, tuple)): node_fns = [node_fns]
+    attrs = {loc: {} for loc in range(64)}
+    for node_fn in node_fns:
+        fn_name = node_fn.__name__
+        attr = node_fn(b)
+        for loc in attrs:
+            attrs[loc].update({fn_name: attr[loc]})
+        
+    nx.set_node_attributes(g, attrs)
+    return g
